ec79443a26ae5e0bd0203c927bbd7b1ef402370c
Maryna Khmarska <111706884+myhappyday@users.noreply.github.com>
2023-06-29 05:03:42 +0300

    Update pattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -19,10 +19,6 @@ class ContactForm extends Component {
 
   onFormSubmit = event => {
     event.preventDefault();
-    // console.log('state:', this.state);
-    // setTimeout(() => {
-    //   console.log('state 1:', this.state);
-    // }, 1000);
 
     // Доступ до даних форми під час її сабміту
     this.props.onSubmit(this.state);
@@ -45,7 +41,7 @@ class ContactForm extends Component {
           <Input
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[A-Za-z\u0080-\uFFFF ']+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             value={this.state.name}
@@ -58,7 +54,7 @@ class ContactForm extends Component {
           <Input
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="^(\+?[0-9.\(\)\-\s]*)$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             value={this.state.number}
